4d5a5a10d9564ce7d0187e02a3361e7c3ddce5ca
beko123X <bekooobdu@gmail>
2026-03-23 02:28:47 +0200

    update the main.jsx route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import './index.css';;
 // قم بتغيير HashRouter إلى BrowserRouter
 import { BrowserRouter } from "react-router-dom" 
 import { Toaster } from 'react-hot-toast';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // في Netlify، المسار الأساسي دائماً هو '/'
-  base: '/', 
+  base: '/', // يجب أن تكون '/' للعمل على Netlify
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-    host: true
   }
 })
